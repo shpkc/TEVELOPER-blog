f92f08ac05ed856f716577ff8ae16206944406fd
Park Si Hyung <shpkc@naver.com>
2022-01-22 12:47:06 +0900

    dev : change main img

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { Link, StaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
-import { Navigation } from ".";
-import config from "../../utils/siteConfig";
+import { BsInstagram } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+
 import "../../styles/app.css";
 
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
@@ -23,31 +23,13 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                         className="site-head"
                         style={{
                             ...(site.cover_image && {
-                                backgroundImage: `url(${site.cover_image})`,
+                                backgroundImage: `url("/images/main.jpeg")`,
                             }),
                         }}
                     >
                         <div className="container">
                             <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link to="/">
-                                        {site.logo ? (
-                                            <img
-                                                className="site-logo"
-                                                src={site.logo}
-                                                alt={site.title}
-                                            />
-                                        ) : (
-                                            <Img
-                                                fixed={
-                                                    data.file.childImageSharp
-                                                        .fixed
-                                                }
-                                                alt={site.title}
-                                            />
-                                        )}
-                                    </Link>
-                                </div>
+                                <div className="site-mast-left"></div>
                                 <div className="site-mast-right">
                                     <a
                                         href="https://www.instagram.com/tevelop_tennis/"
@@ -55,31 +37,36 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
-                                        <img
-                                            className="site-nav-icon"
-                                            src="/images/icons/insta.png"
-                                            alt="Twitter"
-                                        />
+                                        <BsInstagram color="white" size={20} />
+                                    </a>
+                                    <a
+                                        href="https://www.linkedin.com/in/%EC%8B%9C%ED%98%95-%EB%B0%95-1a8805211/"
+                                        className="site-nav-item"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <SiLinkedin color="white" size={20} />
                                     </a>
                                 </div>
                             </div>
                             {isHome ? (
                                 <div className="site-banner">
                                     <h1 className="site-banner-title">
-                                        {site.title}
+                                        테니스 치는 개발자
                                     </h1>
                                     <p className="site-banner-desc">
-                                        {site.description}
+                                        TEVELOPER
                                     </p>
                                 </div>
                             ) : null}
                             <nav className="site-nav">
                                 <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation
-                                        data={site.navigation}
-                                        navClass="site-nav-item"
-                                    />
+                                    <a class="site-nav-item" href="/dev">
+                                        Dev
+                                    </a>
+                                    <a class="site-nav-item" href="/tennis">
+                                        Tennis
+                                    </a>
                                 </div>
                                 <div className="site-nav-right">
                                     <Link
@@ -104,22 +91,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2022 &mdash;
-                                Published with{" "}
-                                <a
-                                    className="site-foot-nav-item"
-                                    href="https://ghost.org"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Ghost
-                                </a>
-                            </div>
-                            <div className="site-foot-nav-right">
-                                <Navigation
-                                    data={site.navigation}
-                                    navClass="site-foot-nav-item"
-                                />
+                                <Link to="/">© 2022 Teveloper</Link>
                             </div>
                         </div>
                     </footer>
