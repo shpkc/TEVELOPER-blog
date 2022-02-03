6b47315e947865654e0093d83f856527e60d2922
Park Si Hyung <shpkc@naver.com>
2022-02-03 09:31:15 +0900

    dev : change css

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { Layout, PostCard, Pagination } from "../components/common";
-import { Head } from "../components/head";
 
 const Index = ({ data, pageContext }) => {
     const posts = data.allContentfulBlog.nodes;
@@ -10,7 +8,6 @@ const Index = ({ data, pageContext }) => {
     return (
         <>
             <Layout isHome={true}>
-                <Head title={"테니스 치는 개발자"} />
                 <div className="container">
                     <section className="post-feed">
                         {posts.map((item) => (
